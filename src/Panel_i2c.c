a556d443aaaf148e8ce7bcea07b6781d30902cf2
Andy Snawerdt <andysnawerdt@gmail.com>
2015-03-28 22:12:43 -0500

    created a Pane_i2c.c file

---
+++ src/Panel_i2c.c
@@ -0,0 +1,38 @@
+/**
+ * @addtogroup I2C
+ * @{
+ * @file      Panel_i2c.c
+ * @author    Iowa State University uCube team
+ * @author    Andy Snawerdt
+ * @author    Jeramie Vens
+ * @author    Kris Burney
+ * @date      Mar 28, 2015
+ * @brief     I2C functions
+ * @copyright Copyright (C) 2015 ISU uCube team
+ * @details   This file contains all the I2C functions such as initialization,
+ *            sending, and recieving data from the I2C bus.
+ */
+
+/*
+    Panel-display
+    Copyright (C) 2015  Iowa State University uCube team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+
+/// @}
+
